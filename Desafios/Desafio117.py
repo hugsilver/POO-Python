95502447a3994a2cd7735c4ab79b06cda04a9daf
Hugo Silva <developer.hugosilva@gmail.com>
2026-03-12 01:10:39 -0300

    Commit do Desafio 117
    Finalizar:
    Faltou formatar com traço
    Faltou preço
    Faltou formatar preço
    Faltou Centralizar

---
+++ Desafios/Desafio117.py
@@ -0,0 +1,33 @@
+from rich import print
+from rich.panel import Panel
+
+#Crie uma classe Produto com nome e preço
+#Crie um método que crie uma etiqueta de produto
+
+def traço():
+    return print('-'*28)
+
+class Produto:
+
+    def __init__(self, nome_prod='vazio', preço=0): #Método Construtor
+        self.nome_prod = nome_prod
+        self.preço = preço
+        #print(f'{self.nome_prod}')
+
+    def etiqueta(self):
+        
+        #Caixa = Panel('[white]{self.nome}}[/]', title='Produto', style='red')
+        #t = traço()
+        print(Panel(f'[white]{self.nome_prod}[/]', title='Produto', style='red', width=30))
+
+    
+prod1 = Produto('MacBook', 6_000)
+prod1.etiqueta()
+#print(prod1)
+    
+
+
+
+
+
+
