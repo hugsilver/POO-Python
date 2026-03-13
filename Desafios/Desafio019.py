aced354a906f1fcbfe349519c9e2c1d0878a27d4
Hugo Silva <developer.hugosilva@gmail.com>
2026-03-13 18:01:23 -0300

    Comitt do Desafio 019
    Okay
    Consegui tudo

--- Desafios/Desafio019.py
+++ Desafios/Desafio019.py
@@ -4,26 +4,28 @@
 
 from rich import print
 from rich.panel import Panel
+from time import sleep
 
-class livro:
-    def __init__(self, nome, pag=0):
-        self.capa = nome
-        self.tp = pag
-        self.pgat = 1
+class livro: 
+    def __init__(self, nome, pag=0): #Nome do livro e Quantidade de Páginas total
+        self.capa = nome #Instânciando nome
+        self.tp = pag #Instânciando total de páginas
+        self.pgat = 1 #Instânciando página atual, iniciando com 1 sempre
         print(f'\nVocê acabou de abrir o livro [blue]{self.capa}[/] quem tem {self.tp} páginas no total. Agora você está na página 1')
     
-    def passpg(self, num):
-        self.pgat += num
-        if (self.pgat+num)>self.tp:
-            self.sim(self.tp - self.pgat)
+    def passpg(self, num): #Método de passar página, recebendo o número de páginas para passar
+        self.pgat += num #Acumulando a página atual, página atual recebe o acrescimo da qauntidade de páginas formadas
+        if (self.pgat+num)>self.tp: #Condicional, se a soma da página atual mais o incremento for maior que o total de páginas, entra nessa condição
+            self.sim(self.pgat-num, self.tp+1) #Chama a função, passando pg inicial (Atual) e final(Total)
             print(f'\nO livro [blue]{self.capa}[/] tem {self.tp} paginás, você avançou até o final das {self.tp}, [red]NÃO TEM COMO AVANÇAR MAIS QUE ISSO!!![/]')
-        else:
-            self.sim(num)
+        else:#Caso contrario, a função entra nessa condição
+            self.sim(self.pgat- num, self.pgat) #Recebe a quantidade de páginas que é para passar e o atributo final (Pg atual)
             print(f'\nVocê passou {num} páginas, sua página atual é {self.pgat}')
 
-    def sim(self, num):
-        for c in range(self.pgat-num, self.pgat+1):
+    def sim(self, inicio, fim): #Função para criar 'animação'
+        for c in range(inicio, fim): #Recebe inicio (Página atual) e fim(Página atual + num)
             print('Pg.',c , '->', end=' ')
+            sleep(0.5)
             
 pg = livro('Meu mundo invertido', 20)
 pg.passpg(5)
