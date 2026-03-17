9864de1149256aebbd2ba53ef87073df07e85ae1
Hugo Silva <developer.hugosilva@gmail.com>
2026-03-17 17:41:00 -0300

    Atualização do Desafio 22
    #OBS.01:Não está modificando a tela (< , > , - , +), mas deveria aparecer que TV está desligada
    #OBS.02: Ainda não está funcionando a renderização da tela quando TV ligada , mas o controler funciona

--- Desafios/Desafio22.py
+++ Desafios/Desafio22.py
@@ -11,23 +11,25 @@ from rich.progress import Progress, BarColumn, TextColumn
 from rich.console import Console
 
 count = 0
+t = 0
 console = Console()
 
 class Controle:
-    def __init__(self, status=False):
+    def __init__(self):
         self.desliga_tv() #Começa desligado
         self.ch = 1
         self.volu = 1
         self.canais = [1, 2, 3, 4, 5]
     
-    def liga_tv(self):
+    def liga_tv(self):#Método para ligar TV
         console.print(Panel(f"[white]CANAL: [/]{self.mostrar_canais()}\n\nVOLUME = {c1.mostrar_volume()}",title="[ TV ]",width=40))
         return 1
         
-    def desliga_tv(self):
-        print(Panel(':red_circle: [red] - A TV está desligada[/]', title='[ TV ] ', style='white', width=40))
+    def desliga_tv(self):#Método para desligar TV
+        return print(Panel(':red_circle: [red] - A TV está desligada[/]', title='[ TV ] ', style='white', width=40)) #Faz o return do print tela desligada
     
-    def canal(self, func=0):#1Método para trocar de canaç
+    def canal(self, func=0):#Método para trocar de canal
+        #Incremento e decremento
         if func == '>':
             self.ch += 1
         elif func == '<':
@@ -37,10 +39,11 @@ class Controle:
             self.ch = 1 #Limita em 1 e reinciar caso passe de 5
         elif self.ch < 1:
             self.ch = 5
-        console.print(Panel(f"[white]CANAL: [/]{self.mostrar_canais()}\n\nVOLUME = {c1.mostrar_volume()}",title="[ TV ]",width=40))#Aqui mesmo - Deu certo
-        return self.ch
+        #console.print(Panel(f"[white]CANAL: [/]{self.mostrar_canais()}\n\nVOLUME = {c1.mostrar_volume()}",title="[ TV ]",width=40))#Aqui mesmo - Deu certo
+        return self.ch #Precisa retorna com o valor do canal atual
         
     def vol(self, func=0):#Método para aumentar ou diminuir volume
+        #Incremento e decremento
         if func == '+':
             self.volu += 1
         elif func == '-':
@@ -50,9 +53,9 @@ class Controle:
             self.volu = 4 #Limita em 4
         if self.volu < 1:
             self.volu = 1 #Limita em 1
-        return self.volu
+        return self.volu #Precisa retorna com o valor do volume atual
     
-    def mostrar_volume(self):#Método para representar a barra de columw
+    def mostrar_volume(self):#Método para representar a barra de colume
         barra = "█" * self.volu + "░" * (4 - self.volu)
         return f"[green]{barra}[/]"
     
@@ -64,12 +67,27 @@ class Controle:
             else:
                 texto += f"[white]{c}[/] "
         return texto
+    
+    def tratar_input(self):
+        permitido = {'<', '>', '-', '+', '@', '0'}
+        
+        try:
+            r = input(f'< CH >{c1.canal()} - VOL +{c1.vol()} ')
+            
+            if len(r) != 1 or r not in permitido:
+                raise ValueError("Entrada inválida")
+            
+            return r
+
+        except ValueError:
+            self.desliga_tv()
+            return None
             
 c1 = Controle() #Instanciando objeto com a classe
 
-while True:#Laçõ condicional infinito
-    #print(f'1° CONTADOR: {count}')#Contadores estão funcionando
-    r = input(f'< CH >{c1.canal()} - VOL +{c1.vol()} ')  
+while True:#Laço condicional infinito
+    #r = input(f'< CH >{c1.canal()} - VOL +{c1.vol()} ')
+    r = c1.tratar_input()  
     if r == '@':
         count += 1
         if count%2 != 0:
@@ -86,4 +104,5 @@ while True:#Laçõ condicional infinito
     if r == '0':
         break
 
-#OBS.01:Está dando erro com 0 ou com qualquer outro comando (< , > , - , +) quando desligada - Ajustar
+#OBS.01:Não está modificando a tela (< , > , - , +), mas deveria aparecer que TV está desligada
+#OBS.02: Ainda não está funcionando a renderização da tela quando TV ligada
